9d642903ab7c82a3889e172ddcc258bc0da163ce
alamin2811 <alaminbinmonsur@gmail.com>
2025-01-26 14:34:39 +0600

    modified

--- src/Component/App/Home/Roadmap.jsx
+++ src/Component/App/Home/Roadmap.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const Roadmap = () => {
   return (
-    <div className='erorr-page'><span>404</span> <br />not found</div>
+    <div className='roadmap-sectoin'>
+      <div className="container">
+        <div className="section-title text-center">
+          <h2>Roadmap</h2>
+          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
